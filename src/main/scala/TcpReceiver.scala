efa9b2dad715c14e5b7b75077a42f35a17a7d0bd
mwilson <mwilson@processor44.com>
2016-03-13 11:38:32 -0600

    Better Apache 2.0 file headers

--- src/main/scala/TcpReceiver.scala
+++ src/main/scala/TcpReceiver.scala
@@ -1,9 +1,19 @@
 /*
- * Mark C Wilson Inc
- * Apache 2 License
- *
- * http://doc.akka.io/docs/akka/2.4.1/scala/io-tcp.html
- */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 import akka.actor._
 
@@ -21,6 +31,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * This actor self terminates upon receiving Tcp.Received(data) or Tcp.PeerClosed
  *
  * Bytes to String, sends to Kafka
+ * http://doc.akka.io/docs/akka/2.4.1/scala/io-tcp.html
  */
 class TcpDataHandler(remote: InetSocketAddress) extends Actor with ActorLogging {
 
--- src/test/scala/TcpReceiverSpec.scala
+++ src/test/scala/TcpReceiverSpec.scala
@@ -1,3 +1,20 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 import akka.actor._
 import akka.testkit.{ImplicitSender, TestKit, TestActorRef}
@@ -25,7 +42,6 @@ class TcpReceiverSpec(_system: ActorSystem)
   }
 
   "A TcpReceiver" should "construct with host port" in {
-    val victim = TestActorRef(Props[KnockKnockJokeParticipant])
     val receiverActor = TestActorRef(Props(new TcpBoundReceiver(Models.RECEIVER_HOST, Models.RECEIVER_PORT)))
     assert(receiverActor.underlyingActor.asInstanceOf[TcpBoundReceiver].host == Models.RECEIVER_HOST)
     assert(receiverActor.underlyingActor.asInstanceOf[TcpBoundReceiver].port == Models.RECEIVER_PORT)
